7e8ab1af7c1474cf75be5c697aef83d4b7b25df6
wswatsonws <504611175@qq.com>
2018-11-16 17:13:50 +0100

    1 commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SiderDemo from './Navi/Navi'
+//import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<SiderDemo />, document.getElementById('root'));
+//registerServiceWorker();
